b834d4786d2459813f288f132ca7e29df32dabb7
Pranavan Krishnamoorthy <pranavan.krishna23@gmail.com>
2024-05-22 09:24:49 -0400

    finalized changes to crawler class and added method to save data to csv

--- core/crawler.js
+++ core/crawler.js
@@ -1,69 +1,16 @@
-const ObjectsToCsv = require('objects-to-csv');
-const { findAll } = require("../utils/index");
-
 class Crawler {
-    constructor(selectors) {
-        /*
-            selectors: object<string, string>
-
-            The crawler objects takes in selectors and can scrape given urls with the crawl() method.
-            selectors requrie a key value pair, e.x {"Name": <css path>, "Email": <css path>}
-        */
-        this.stored = []; // not used now, but can be used to save stored values to csv file
-        this.selectors = selectors;
+    constructor() {
         this.visited = [];
         this.hrefs = [];
     }
 
-    addValue(obj) {
-        this.stored.push(obj)
-    }
-
-    get getStoredAll() {
-        return this.stored
-    }
-
-    getStored(key) {
-        return this.stored[key]
-    }
-
-    async crawl(page, link) {
-        /*
-            SHOULD BE REMOVED!
-            page: page object
-            link: string
-    
-            go to the link provided and scrape the title and email addres of the club
-            once data is scraped, return to starting url
-        */
-        await page.goto(link);
-
-        let data = {} // data will be an object with the keys of the passes selectors
-        for (const [key, value] of Object.entries(this.selectors)) {
-            try {
-                const selector = await page.waitForSelector(value, {timeout: 200});
-                if (key == "email") {
-                    const content = await selector?.evaluate(el => el.getAttribute("href"));
-                    data[key] = content;
-                }
-                else {
-                    const content = await selector?.evaluate(el => el.textContent);
-                    data[key] = content;
-                }
-            } catch {
-                data[key] = "n/a";
-            }
-        };
-
-        this.addValue(data);
-        await page.goBack();
-    };
-
     async parse(page, callback, depth=3, default_val=undefined) {
         // call the callback
         // read all the hrefs on the page and visited each one
+
+        // if depth is reached and nothing is found return default_val to prevent crawler from wandering to far
         if (depth == 0) {
-            return "n/a";
+            return default_val;
         }
 
         const result = callback();
@@ -71,19 +18,11 @@ class Crawler {
             return result;
         }
 
-        // const gatheredHrefs = await page.page.evaluate(() => {
-        //     let links = [];
-        //     let elements2 = document.querySelectorAll('a');
-        //     for (let element2 of elements2)
-        //         links.push(element2.href);
-        //     return links.filter(el => el.includes("contact"));
-        // });
-
         const gatheredHrefs = await page.findAll("a"); // all elements are of tpye Element
 
         this.hrefs.push(...gatheredHrefs);
 
-        let found = default_val;
+        let found = default_val; // the default val is what is retured if nothing is found, set to undefined recommended to be "n/a"
         for (let i = 0; i < this.hrefs.length; i++) {
             let current_href = await this.hrefs[i].href
             if (this.visited.includes(current_href)) {
@@ -94,20 +33,6 @@ class Crawler {
             found = await this.parse2(page, callback, depth - 1); // return the call back function
         }
         return found;
-    }
-
-    async saveAsCsv(path) {
-        /*
-            saves the stored data in the this.stored attribute to a csv file of the given name
-
-            args:
-                path: string
-            returns:
-                none
-        */
-        const csv = new ObjectsToCsv(this.stored);
-
-        await csv.toDisk(path);
     }
 }
 
